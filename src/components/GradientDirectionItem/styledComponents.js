acae05fcee59e81f842cb55af8b9d79d826e1c9a
Nitin <nitinpathak337@gmail.com>
2022-10-20 14:40:35 +0000

    solution

---
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -0,0 +1,19 @@
+// Style your elements here
+import styled from 'styled-components'
+
+export const ActiveBtn = styled.button`
+  background-color: white;
+  color: #334155;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4px;
+  height: 30px;
+  width: 65px;
+  margin: 13px;
+  opacity: 1;
+`
+
+export const Btn = styled(ActiveBtn)`
+  opacity: 0.5;
+`
